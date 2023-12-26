65612b427407c687b22cff21b93423633b71cf99
Rh4096 <bionicvnb@gmail.com>
2023-12-27 04:10:19 +0700

    add resolution

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,23 @@ impl Mode {
         }
     }
 }
+
+pub enum Resolution {
+    SD,
+    HD,
+    FHD,
+    QHD,
+    UHD,
+}
+
+impl Resolution {
+    pub fn get_resolution(&self) -> (u32, u32) {
+        match self {
+            Resolution::SD => (640, 480),
+            Resolution::HD => (1280, 720),
+            Resolution::FHD => (1920, 1080),
+            Resolution::QHD => (2560, 1440),
+            Resolution::UHD => (3840, 2160),
+        }
+    }
+}
