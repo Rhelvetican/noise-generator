481ff1951586d39a78ed522e721a848eb9a6d625
Rh4096 <bionicvnb@gmail.com>
2023-12-27 04:25:39 +0700

    stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use image;
+use image::RgbImage;
+use std::{
+    env,
+    str,
+    path::PathBuf
+};
+use rand::Rng;
+use noise_generator::*;
+
+const USAGE: &'static str = "Usage: ./noisegen [-arguments] <output location>";
 
 fn main() -> Result<(), ()> {
     
